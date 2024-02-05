82f05d7857731db9e9af7ad9fc61842b1239c8ee
Sam Allen <samallen659@gmail.com>
2024-02-05 19:18:54 +0000

    moves service to interface with implementations for each routing algorithm

--- internal/server/service.go
+++ internal/server/service.go
@@ -1,8 +1,39 @@
 package server
 
-type Service struct {
+import "log"
+
+type Service interface {
+	Serve()
+}
+
+type RoundRobinService struct {
+	Name            string
+	ListenAddr      string
+	RoundRobinCount int
+	Endpoints       []Endpoint
+}
+
+func (rrs *RoundRobinService) Serve() {
+	log.Print("serving round robin: " + rrs.Name)
+}
+
+type LeastConnectionService struct {
+	Name            string
+	ListenAddr      string
+	ConnectionCount map[string]int
+	Endpoints       []Endpoint
+}
+
+func (lcs *LeastConnectionService) Serve() {
+	log.Print("serving least connection: " + lcs.Name)
+}
+
+type IPHashService struct {
 	Name       string
 	ListenAddr string
-	Algorithm  Algorithm
 	Endpoints  []Endpoint
+}
+
+func (ihs *IPHashService) Serve() {
+	log.Print("serving ip hash: " + ihs.Name)
 }
